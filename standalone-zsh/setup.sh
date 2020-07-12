6b4aa21e1e5c0128937f598140e199833d8806e6
ccsss <chansol0505@naver.com>
2020-07-12 19:56:55 +0900

    ohmyzsh

--- standalone-zsh/setup.sh
+++ standalone-zsh/setup.sh
@@ -7,10 +7,12 @@ cp libncursesw.so.6 $STANDALONE_ZSH_PATH
 cp libm.so.6 $STANDALONE_ZSH_PATH
 cp zle.so $STANDALONE_ZSH_PATH
 cp zsh $STANDALONE_ZSH_PATH
+tar xvf ohmyzsh.tar.gz
+cp -r ohmyzsh ~/.oh-my-zsh
 echo alias zsh=~/.standalone/zsh/standalone-zsh.sh >> ~/.aliases
 cat <<EOT > ~/.zshrc
 export TERM="xterm-256color"
-export ZSH="$HOME/.oh-my-zsh"
+export ZSH="~/.oh-my-zsh"
 export INTERFACES="wlp0s20f0u11"
 #ZSH_THEME="alien-minimal/alien-minimal"
 ZSH_THEME="3den"
