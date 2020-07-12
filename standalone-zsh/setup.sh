e2985eac4b1318977b30a7d75cc75a33541b0dd1
ccsss <chansol0505@naver.com>
2020-07-12 20:20:06 +0900

    xterm

--- standalone-zsh/setup.sh
+++ standalone-zsh/setup.sh
@@ -7,23 +7,10 @@ cp libncursesw.so.6 $STANDALONE_ZSH_PATH
 cp libm.so.6 $STANDALONE_ZSH_PATH
 cp zle.so $STANDALONE_ZSH_PATH
 cp zsh $STANDALONE_ZSH_PATH
-# tar xvf ohmyzsh.tar.gz
-# cp -r .oh-my-zsh ~
-if [[ ! -d ~/.oh-my-zsh ]]; then
-    wget -q -O install_ohmyzsh.sh https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
-    # CHSH=no RUNZSH=no sh install_ohmyzsh.sh
-    sh install_ohmyzsh.sh --unattended
-    rm install_ohmyzsh.sh
-fi
-# [[ -f ~/.zshrc ]] && mv ~/.zshrc ~/.zshrc.bak
-# cp _zshrc ~/.zshrc
-[[ ! -d ~/.oh-my-zsh/custom/themes/alien-minimal ]] && \
-    git clone -q --recurse-submodules https://github.com/eendroroy/alien-minimal.git \
-        ~/.oh-my-zsh/custom/themes/alien-minimal
-[[ ! -d ~/.oh-my-zsh/plugins/zsh-autosuggestions ]] && \
-    git clone -q https://github.com/zsh-users/zsh-autosuggestions \
-        ~/.oh-my-zsh/plugins/zsh-autosuggestions
-
+tar xvf ohmyzsh.tar.gz
+cp -r .oh-my-zsh ~
+mkdir ~/.terminfo
+cp xterm-256color ~/.terminfo/
 echo alias zsh=~/.standalone/zsh/standalone-zsh.sh >> ~/.aliases
 cat <<EOT > ~/.zshrc
 export TERM="xterm-256color"
@@ -35,7 +22,7 @@ plugins=(
   z
   zsh-autosuggestions
 )
-source $ZSH/oh-my-zsh.sh
+source ~/.oh-my-zsh/oh-my-zsh.sh
 stty -ixon
 source ~/.aliases
 EOT
