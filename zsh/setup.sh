0aa703f068513bcd5ade1b7c4956b40e13ca426f
ccsss <chansol0505@naver.com>
2020-07-13 14:58:57 +0900

    zsh

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -6,6 +6,7 @@ cp zsh $STANDALONE_ZSH_PATH
 cp zsh-bin $STANDALONE_ZSH_PATH
 cp lib $STANDALONE_ZSH_PATH -r
 cp functions $STANDALONE_ZSH_PATH -r
+cp site-functions $STANDALONE_ZSH_PATH -r
 cp libncursesw.so.6 $STANDALONE_ZSH_PATH
 cp libm.so.6 $STANDALONE_ZSH_PATH
 tar xvf ohmyzsh.tar.gz
@@ -17,7 +18,8 @@ cp xterm-256color ~/.terminfo/x/
 cat <<EOT > ~/.zshrc
 export TERM="xterm-256color"
 export ZSH="~/.oh-my-zsh"
-export FPATH=\$FPATH:~/.standalone/zsh/functions
+export FPATH="\$FPATH:export FPATH="/home/ccsss/.oh-my-zsh/plugins/zsh-autosuggestions:/home/ccsss/.oh-my-zsh/plugins/z:/home/ccsss/.oh-my-zsh/functions:/home/ccsss/.oh-my-zsh/completions:/home/ccsss/.standalone/zsh/site-functions:/home/ccsss/.standalone/zsh/site-functions:/home/ccsss/.standalone/zsh/functions/Calendar:/home/ccsss/.standalone/zsh/functions/Chpwd:/home/ccsss/.standalone/zsh/functions/Completion:/home/ccsss/.standalone/zsh/functions/Completion/Base:/home/ccsss/.standalone/zsh/functions/Completion/Linux:/home/ccsss/.standalone/zsh/functions/Completion/Unix:/home/ccsss/.standalone/zsh/functions/Completion/X:/home/ccsss/.standalone/zsh/functions/Completion/Zsh:/home/ccsss/.standalone/zsh/functions/Exceptions:/home/ccsss/.standalone/zsh/functions/Math:/home/ccsss/.standalone/zsh/functions/MIME:/home/ccsss/.standalone/zsh/functions/Misc:/home/ccsss/.standalone/zsh/functions/Newuser:/home/ccsss/.standalone/zsh/functions/Prompts:/home/ccsss/.standalone/zsh/functions/TCP:/home/ccsss/.standalone/zsh/functions/VCS_Info:/home/ccsss/.standalone/zsh/functions/VCS_Info/Backends:/home/ccsss/.standalone/zsh/functions/Zftp:/home/ccsss/.standalone/zsh/functions/Zle"
+export PATH=$PATH:~/.standalone/zsh
 #ZSH_THEME="alien-minimal/alien-minimal"
 ZSH_THEME="3den"
 plugins=(
