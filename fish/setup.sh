8688f840a3c558b7223ae6ab019c27c1e65f6edb
ccsss <chansol0505@naver.com>
2020-07-13 14:15:06 +0900

    fish

--- fish/setup.sh
+++ fish/setup.sh
@@ -3,6 +3,6 @@ set -xe
 STANDALONE_FISH_PATH=~/.standalone/fish
 mkdir $STANDALONE_FISH_PATH -p
 cp standalone-fish.sh $STANDALONE_FISH_PATH
-cp libncursesw.so.6 $STANDALONE_FISH_PATH
+cp lib* $STANDALONE_FISH_PATH
 cp fish $STANDALONE_FISH_PATH
 echo alias fish=~/.standalone/fish/standalone-fish.sh >> ~/.aliases
