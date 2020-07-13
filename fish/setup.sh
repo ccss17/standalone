00e0b37d78b97932a564012f2795ca5b79a05407
ccsss <chansol0505@naver.com>
2020-07-13 14:10:57 +0900

    fish

--- fish/setup.sh
+++ fish/setup.sh
@@ -5,9 +5,3 @@ mkdir $STANDALONE_FISH_PATH -p
 cp standalone-fish.sh $STANDALONE_FISH_PATH
 cp libncursesw.so.6 $STANDALONE_FISH_PATH
 cp fish $STANDALONE_FISH_PATH
-tar xvf ohmyfish.tar.gz
-if [[ ! -d ~/.oh-my-fish ]]; then
-    cp -r .oh-my-fish ~
-fi
-mkdir ~/.terminfo/x -p
-cp xterm-256color ~/.terminfo/x/
