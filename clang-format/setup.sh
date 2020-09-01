2e08735f48d67bc6a464f4af2fb29045b6739f3d
ccsss <chansol0505@naver.com>
2020-09-01 18:11:54 +0900

    clang-format

--- clang-format/setup.sh
+++ clang-format/setup.sh
@@ -4,6 +4,7 @@ STANDALONE_CLANGFORAMT_PATH=~/.standalone/clang-format
 mkdir $STANDALONE_CLANGFORAMT_PATH -p
 cp standalone-clang-format.sh $STANDALONE_CLANGFORAMT_PATH
 cp libclang-cpp.so.10 $STANDALONE_CLANGFORAMT_PATH
+cp libLLVM-10.so $STANDALONE_CLANGFORAMT_PATH
 cp clang-format $STANDALONE_CLANGFORAMT_PATH
 cp .clang-format ~
 echo alias clang-format=~/.standalone/clang-format/standalone-clang-format.sh >> ~/.zsh_aliases
